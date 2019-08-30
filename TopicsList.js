056f4d303a897a46ff83e7325b15bf3e0eab4c7d
Sid Suresh <sidhdharth.suresh@gmail.com>
2019-08-30 10:13:21 -0500

    Added healthcare

--- TopicsList.js
+++ TopicsList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Platform, TouchableOpacity, Image , View, StyleSheet } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+
 class TopicList extends Component {
     render (){
         return(
@@ -18,6 +19,38 @@ class TopicList extends Component {
             </CardItem>
             <CardItem cardBody>
               <Image source={{uri: 'https://news.utexas.edu/wp-content/uploads/2018/03/gun_control_830-600x337.5-c-default.jpg'}} style={{height: 200, width: null, flex: 1}}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button transparent>
+                  <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+          
+         <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail source={{uri: 'Image URL'}} />
+                <Body>
+                  <Text>Healthcare</Text>
+                  <Text note></Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image source={{uri: 'https://www.techworld.com/cmsdata/features/3668601/gp-at-hand-nhs-ai-health-1000x600_thumb800.jpg'}} style={{height: 200, width: null, flex: 1}}/>
             </CardItem>
             <CardItem>
               <Left>
