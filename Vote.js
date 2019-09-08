945b758a93cef0b1a1f72fc8a06576f5a596d652
Michael Gathara <mikegtrm@gmail.com>
2019-09-08 00:10:10 -0500

    Last one for the night. Changed a few things to be better maintained.

--- Vote.js
+++ Vote.js
@@ -1,46 +1,41 @@
 import React, {Component} from 'react';
-import { Platform, TouchableOpacity, Image , View, StyleSheet } from 'react-native';
-import {Container, Header, Body, CheckBox, Title, Card, CardItem,
-Left, Right, Icon, Content, Thumbnail, Grid, Col, Text, Button} from 'native-base';
+import { Platform, TouchableOpacity, Image , StyleSheet } from 'react-native';
+import {Container, Header, Body, CheckBox, Title, Card, CardItem,Left, Right, Icon, Content, Thumbnail, Grid, Col, Button} from 'native-base';
 import { StatusBar } from 'react-native'
- class VoteScreen extends React.Component {
+import ReactNative from 'react-native'
+const {View,Text,} = ReactNative 
+class VoteScreen extends React.Component {
   render() {
-//     return(
-//         <StatusBar backgroundColor="blue" barStyle = 'light-content' />
-//       <Container style={styles.container}>
-//           <Header style={styles.header}>
-              
-//               <Body style={{flex:1}}>
-//                   <Title>Vote</Title>
-//               </Body>
-              
-//           </Header>
-//           <Content contentContainerStyle={styles.content} >  
-//           </Content>
-//       </Container>
-//   );
     return (
-        <StatusBar backgroundColor="blue" barStyle = 'light-content' />
-
+      <React.View
+        style={{
+          flexDirection: 'row',
+          height: 100,
+          padding: 20,
+        }}>
+        <StatusBar backgroundColor="blue" barStyle = 'dark-content' />
+        <View style={{backgroundColor: 'blue', flex: 0.3}} />
+        <View style={{backgroundColor: 'red', flex: 0.5}} />
+        <Text>Hello World!</Text>
+      </React.View>
     );
- }
-
+  }
 }
 
 export default VoteScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 5
-  },
-  header: {
-      paddingRight: 15,
-      paddingLeft: 15
-  },
-  content: {
-      display: "flex",
-      flex: 1,
-      justifyContent: "center",
-      padding: 15
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     paddingTop: 5
+//   },
+//   header: {
+//       paddingRight: 15,
+//       paddingLeft: 15
+//   },
+//   content: {
+//       display: "flex",
+//       flex: 1,
+//       justifyContent: "center",
+//       padding: 15
+//   }
+// });
