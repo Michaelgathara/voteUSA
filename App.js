d43c356c7218faa06a4ed4f9ec7bc9326430fc3f
Sybersid <sidhdharth.suresh@gmail.com>
2019-09-28 08:22:01 -0500

    voteUSA

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs'; 
+import CanidateScreen from './Canidates'
+import TopicScreen from './Topics'
+import VoteScreen from './Vote'
+
+
+const TabNavigator = createBottomTabNavigator({
+  Canidates: { screen: CanidateScreen,
+    navigationOptions: {
+      tabBarLabel: 'Canidates',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-contact" color={tintColor} size={24} />
+      )
+    }
+  },
+  Topics:  { screen: TopicScreen,
+    navigationOptions: {
+      tabBarLabel: 'Topics',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-list" color={tintColor} size={24} />
+      )
+    }
+  },
+  Vote:  { screen: VoteScreen,
+    navigationOptions: {
+      tabBarLabel: 'Vote',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-information-circle" color={tintColor} size={24} />
+      )
+    }
+  }
+  },
+{
+  initialRouteName : 'Vote',
+  order: ['Canidates', 'Topics', 'Vote'],
+
+  navigationOptions: {
+    tabBarVisible: true
+  },
+  tabBarOptions: {
+    activeTintColor: 'crimson',
+    inactiveTintColor: 'dodgerblue'
+  }
+});
+
+export default createAppContainer(TabNavigator);
---
+++ Topics.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import { Platform, ScrollView, TouchableOpacity, Image , View, StyleSheet } from 'react-native';
+import {Container, Header, Body, CheckBox, Title, Card, CardItem,
+Left, Right, Icon, Content, Thumbnail, Grid, Col, Text, Button} from 'native-base';
+import TopicsList from './TopicsList';
+
+class TopicScreen extends React.Component {
+  render() {
+    return(
+      <Container style={styles.container}>
+          <Header style={styles.header}>
+              <Body style={{flex:1}}>
+                  <Title>Topics</Title>
+              </Body>
+          </Header>
+          <Content contentContainerStyle={styles.content} >  
+         <ScrollView contentContainerStyle={styles.contentContainer}>
+         <TopicsList/>
+        </ScrollView>
+          </Content>
+      </Container>
+  );
+ }
+
+}
+
+export default TopicScreen;
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  header: {
+      paddingRight: 15,
+      paddingLeft: 15
+  },
+  content: {
+      display: "flex",
+      flex: 1,
+      justifyContent: "center",
+      padding: 0
+  }
+});
